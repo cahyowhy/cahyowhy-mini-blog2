93322818dcf22da1729a6e4aa83564690d5dde9f
cahyowhy <cahyowhy01@gmail.com>
2017-08-24 01:39:41 +0700

    dawdawd

--- cahyowhy-mini-blog-frontend/app/routes/post/category.js
+++ cahyowhy-mini-blog-frontend/app/routes/post/category.js
@@ -1,30 +1,92 @@
 import Ember from 'ember';
 import ENV from '../../config/environment';
+import BaseRouter from '../base';
+import Post from '../../entity/post';
 
-export default Ember.Route.extend({
-  beforeModel(){
-    this.debug(this.controllerFor('post.category').set('posts', []));
-  },
+const {computed} = Ember;
+export default BaseRouter.extend({
+  offset: ENV.APP.DEFAULT_OFFSET,
+  posts: [],
+  controller: null,
   model(param){
-    const query = {
-      offset: ENV.APP.DEFAULT_OFFSET,
-      limit: ENV.APP.DEFAULT_LIMIT,
-      category: param.id
-    };
+    this.offset = ENV.APP.DEFAULT_OFFSET;
+    let post = new Post().getInitializeValue();
+    post.post.category = param.id;
+    post.post.offset = ENV.APP.DEFAULT_OFFSET;
+    post.post.limit = ENV.APP.DEFAULT_LIMIT;
+    post = new Post().getValue(post);
 
     return Ember.RSVP.hash({
-      posts: this.postService.find(query),
+      posts: this.postService.find(post),
       category: this.category
     });
   },
-  afterModel(model){
-    this.controllerFor('post.category').set('posts', model.posts);
-    this.controllerFor('post.category').set('category', model.category);
+  afterReder(){
+    this._super(...arguments);
+    /**
+     * alsco check this
+     */
+    this.controller.set('ifPostIsEmpty', computed.equal('posts.length', 0));
+  },
+  getTitlePost(){
+    const category = parseInt(this.controller.get("category"));
+
+    return category === 1 ? "Postingan kategori sains" :
+      category === 2 ? "Postingan kategori komedi" :
+        category === 3 ? "Postingan kategori sejarah" :
+          category === 4 ? "Postingan kategori gosip" :
+            category === 5 ? "Postingan kategori puisi" :
+              category === 6 ? "Postingan kategori pribadi" :
+                category === 7 ? "Postingan kategori mistis" :
+                  category === 8 ? "Postingan kategori berita" :
+                    category === 9 ? "Postingan kategori teknologi" :
+                      "Postingan terbaru";
+  },
+  getPengantarPost(){
+    const category = parseInt(this.controller.get("category"));
+
+    return category === 1 ? "Postingan berisi tentang ilmu sains dan alam" :
+      category === 2 ? "Postingan kocak, bergairah dan tidak membosankan" :
+        category === 3 ? "Postingan berisi sejarah tentang apapum" :
+          category === 4 ? "Gosip hot tentang artis dan selebriti" :
+            category === 5 ? "Puisi terfavorit yang hari ini" :
+              category === 6 ? "Postingan kategori pribadi" :
+                category === 7 ? "Cerita Mistis, bikin meringis untuk menemani malammu yanf sendiri" :
+                  category === 8 ? "Berita terbaru dan menyegarkan untuk dibaca" :
+                    category === 9 ? "Perkembangan teknologi terbaru hari ini" :
+                      "Postingan terbaru yang menyegarkan";
+  },
+  setupController(controller, model){
+    this.controller = controller;
+    this.posts = model.posts;
+    this.controller.set('posts', this.posts);
+    this.controller.set('category', model.category);
+    this.controller.set('title', this.getTitlePost());
+    this.controller.set('pengantar', this.getPengantarPost());
   },
   actions: {
     willTransition(){
       Ember.$('#post-container').off();
       Ember.$('#post-container').removeAttr('style');
+    },
+    onLoadPost(){
+      const context = this;
+      const category = this.controller.get("category");
+      this.offset = this.offset + ENV.APP.DEFAULT_LIMIT;
+
+      let post = new Post().getInitializeValue();
+      post.post.category = category;
+      post.post.offset = this.offset;
+      post.post.limit = ENV.APP.DEFAULT_LIMIT;
+      post = new Post().getValue(post);
+
+      this.doFind("post", post).then(function (results) {
+        results.forEach(function (item) {
+          context.posts.pushObject(item);
+          context.controller.get('posts').pushObject(item);
+        });
+        context.controller.set('ifPostIsEmpty', results.length === 0);
+      });
     }
   }
 });
--- cahyowhy-mini-blog-frontend/app/routes/post/index.js
+++ cahyowhy-mini-blog-frontend/app/routes/post/index.js
@@ -1,18 +1,43 @@
 import Ember from 'ember';
 import ENV from '../../config/environment';
+import BaseRouter from '../base';
+import offsetlimit from '../../entity/offsetlimit';
 
-export default Ember.Route.extend({
+export default BaseRouter.extend({
+  offset: ENV.APP.DEFAULT_OFFSET,
+  posts: [],
+  controller: null,
+  setupController(controller, model){
+    this.controller = controller;
+    this.posts = model.posts;
+    this.controller.set('posts', this.posts);
+  },
+  afterReder(){
+    this._super(...arguments);
+    /**
+     * alsco check this
+     */
+    this.controller.set('ifPostIsEmpty', Ember.computed.equal('posts.length', 0));
+  },
   model(){
-    const query = {
-      offset: ENV.APP.DEFAULT_OFFSET,
-      limit: ENV.APP.DEFAULT_LIMIT
-    };
-
+    this.offset = ENV.APP.DEFAULT_OFFSET;
     return Ember.RSVP.hash({
-      posts: this.postService.find(query)
+      posts: this.postService.find(offsetlimit())
     });
   },
-  afterModel(model){
-    this.controllerFor('post.index').set('posts', model.posts);
+  actions: {
+    onLoadPost(){
+      const context = this;
+      this.offset = this.offset + ENV.APP.DEFAULT_LIMIT;
+      const query = offsetlimit(this.offset);
+
+      this.doFind("post", query).then(function (results) {
+        results.forEach(function (item) {
+          context.posts.pushObject(item);
+          context.controller.get('posts').pushObject(item);
+        });
+        context.controller.set('ifPostIsEmpty', results.length === 0);
+      });
+    }
   }
 });
