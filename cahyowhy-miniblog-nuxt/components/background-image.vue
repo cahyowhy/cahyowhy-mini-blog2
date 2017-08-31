e323be9d826c12dd8c477e2ac9ec4112b86763cf
cahyowhy <cahyowhy01@gmail.com>
2017-08-31 20:34:23 +0700

    commit suicide

---
+++ cahyowhy-miniblog-nuxt/components/background-image.vue
@@ -0,0 +1,39 @@
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+    <div v-bind:class="classNames" v-bind:style="image">
+    </div>
+</template>
+<script>
+    export default {
+        props: ['src', 'className'],
+        data(){
+            return {
+                image: {
+                    'background-image': `url("${this.src}")`
+                },
+                classNames: `${this.className} bg-image-container`
+            }
+        },
+        some(){
+        },
+        mounted(){
+            console.log(this);
+            const context = this;
+            if (this.src === undefined || this.src === null || this.src.length === 0) {
+                this.image = {
+                    "background-image": `url(${"/no-image.png"})`
+                };
+            } else {
+                Ember.$('<img/>').attr('src', this.image).on('load', function () {
+                    Ember.$(this).remove();
+                    context.image = {
+                        'background-image': "url(" + context.src + ")"
+                    };
+                }).on('error', function () {
+                    context.image = {
+                        "background-image": `url(${"/no-image.png"})`
+                    };
+                });
+            }
+        }
+    }
+</script>
--- cahyowhy-miniblog-nuxt/components/postItem.vue
+++ cahyowhy-miniblog-nuxt/components/postItem.vue
@@ -4,12 +4,12 @@
             <div class="col-sm-12">
                 <div class="row post-vertical">
                     <div class="pull-left">
-                        <nuxt-link to="{path: 'post-detail', params:{id: idPost}">
-                            <!--{{background-image-container src=img class="img-content"}}-->
+                        <nuxt-link :to="{name: 'post-detail-id', params:{id: idPost}}">
+                            <background :src="img" :classNames="img-content"/>
                         </nuxt-link>
                     </div>
                     <div class="media-body">
-                        <nuxt-link to="post-detail/idPost">
+                        <nuxt-link :to="{name: 'post-detail-id', params:{id: idPost}}">
                             <h3>{{title}}</h3>
                         </nuxt-link>
                         <p class="names">{{username}}, {{createdAt}}</p>
@@ -32,13 +32,13 @@
                         <div class="wow zoomIn pos-relative">
                             <div class="category-post">
                                 <h5>{{category}}</h5></div>
-                            <nuxt-link :to="{path: '/post-detail', params:{id: idPost}}">
-                                <!--{{background-image-container src=img class="bgpost"}}-->
+                            <nuxt-link :to="{name: 'post-detail-id', params:{id: idPost}}">
+                                <background :src="img" className="img-content"/>
                             </nuxt-link>
                         </div>
                     </div>
                     <h4>
-                        <nuxt-link :to="{path: '/post-detail', params: { id:idPost } }">
+                        <nuxt-link :to="{name: 'post-detail-id', params:{id: idPost}}">
                             {{title}}
                         </nuxt-link>
                     </h4>
@@ -53,7 +53,11 @@
     </div>
 </template>
 <script>
+    import background from './background-image.vue';
     export default {
+        components: {
+            'background': background
+        },
         props: ['isVertical', 'likes', 'category', 'img', 'idPost', 'title', 'username', 'createdAt', 'review']
     }
 </script>
